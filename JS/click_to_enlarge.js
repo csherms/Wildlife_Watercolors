9d889498ed318d2c90a0c35b799254cc738150a4
UDAD247 <udadeveryday@gmail.com>
2023-03-12 20:06:47 -0600

    third_commit

---
+++ JS/click_to_enlarge.js
@@ -0,0 +1,42 @@
+document.getElementById("img1").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img2").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img3").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img4").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img5").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img6").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img7").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img8").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img9").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img10").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img11").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img12").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img13").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
+document.getElementById("img14").addEventListener("click", function () {
+  this.classList.toggle("big");
+});
